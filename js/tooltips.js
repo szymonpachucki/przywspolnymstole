f8f6b643545fd94d718d351bce8c16d7158fc4d8
szymonpachucki <45148501+szymonpachucki@users.noreply.github.com>
2018-11-18 18:55:49 +0100

    Add files via upload

---
+++ js/tooltips.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    //Wyœwietlanie podpowiedzi u góry
+    $(".tip-top").tooltip({
+        placement : 'top'
+    });
+    //Wyœwietlanie podpowiedzi u góry
+    $(".tip-right").tooltip({
+        placement : 'right'
+    });
+    //Wyœwietlanie podpowiedzi u góry
+    $(".tip-bottom").tooltip({
+        placement : 'bottom'
+    });
+    //Wyœwietlanie podpowiedzi u góry
+    $(".tip-left").tooltip({
+        placement : 'left'
+    });
+});
